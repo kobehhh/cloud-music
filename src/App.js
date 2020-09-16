103530807f92d2ee4df8f2cebdcaa83683c56574
liangfeng.wang <liangfeng.wang@xuncetech.com>
2020-09-16 16:05:02 +0800

    配置路由

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { renderRoutes } from 'react-router-config';//renderRoutes 读取路由配置转化为 Route 标签
+import { HashRouter } from 'react-router-dom';
+import routes from './routes';
+import { GlobalStyle } from  './style';
 
 function App() {
   return (
-    <div className="App">
-      123
-    </div>
+    <HashRouter>
+      { renderRoutes (routes) }
+    </HashRouter>
   );
 }
 
