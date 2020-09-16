6bdd9844397c68a9e89d62657c2d86083393a3c5
liangfeng.wang <liangfeng.wang@xuncetech.com>
2020-09-16 16:26:56 +0800

    完成redux配置

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { renderRoutes } from 'react-router-config';//renderRoutes 读取路由配置转化为 Route 标签
 import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import store from './store';
 import routes from './routes';
-import { GlobalStyle } from  './style';
+import { GlobalStyle } from './style';
 
 function App() {
   return (
-    <HashRouter>
-      { renderRoutes (routes) }
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
   );
 }
 
